ea57dfa5c412fdd7257b1418125351d623356cbd
MoyankGiri <56188720+MoyankGiri@users.noreply.github.com>
2020-10-25 22:03:40 +0530

    Added a basic sidebar component

---
+++ sidebar_index.js
@@ -0,0 +1,13 @@
+import Sidebar from "temp.js";
+import './sidebar.css';
+ReactDOM.render(
+    <Sidebar width={300} height={300}>
+          <h1>Nav Item</h1>
+          <h1>Nav Item</h1>
+          <h1>Nav Item</h1>
+          <h1>Nav Item</h1>
+          <h1>Nav Item</h1>
+    </Sidebar>,
+  document.getElementById('root')
+);
+reportWebVitals();
